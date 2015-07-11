28af68863c94907f5cd6bfa8e88258814b374237
amerriman <amm434@gmail.com>
2015-07-11 14:54:37 -0600

    updated solution to include more choice

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
 window.onload = function() {
 
-var temp = prompt("What temperature do you need to convert?");
-var tempF = temp*9/5+32;
-alert("The temperature in Farenheit is " + tempF);
+var temp = prompt("Would you like to find a temperature in C, or F?");
+
+if (temp.toUpperCase() === "C"){
+  var tempC = prompt("Enter the temperature in Farenheit.");
+    var tempCC = (tempC-32)*5/9;
+      alert("The temperature in Celcius is " + tempCC);}
+
+else if (temp.toUpperCase()==="F") {
+  var tempF = prompt ("Enter the temperature in Celcius.");
+    var tempFF = tempF*9/5+32;
+      alert("The temperature in Farenheit is " + tempFF);}
+
+else {
+  alert("That was not one of the options.")
+}
 
 }
