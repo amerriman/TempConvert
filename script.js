2f54e14297e822e1b0d38a370f6797865956b7ab
amerriman <amm434@gmail.com>
2015-07-11 14:23:07 -0600

    Added temp conversion code

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
-window.onload = function {
+window.onload = function() {
 
-  
+var temp = prompt("What temperature do you need to convert?");
+var tempF = temp*9/5+32;
+alert("The temperature in Farenheit is " + tempF);
+
 }
